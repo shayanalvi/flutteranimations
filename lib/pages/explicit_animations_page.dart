bc953272bf82da8c4f51fe99f7f2f8228a17c768
shayanalvi <shayanalvi6@gmail.com>
2024-11-06 21:36:46 +0500

    explicit animations added

--- lib/pages/explicit_animations_page.dart
+++ lib/pages/explicit_animations_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class ExplicitAnimationsPage extends StatefulWidget {
@@ -7,14 +9,87 @@ class ExplicitAnimationsPage extends StatefulWidget {
   State<ExplicitAnimationsPage> createState() => _ExplicitAnimationsPageState();
 }
 
-class _ExplicitAnimationsPageState extends State<ExplicitAnimationsPage> {
+class _ExplicitAnimationsPageState extends State<ExplicitAnimationsPage>
+    with TickerProviderStateMixin {
+  late AnimationController _animationController1;
+
+  late Animation<double> _scaleAnimation;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _animationController1 = AnimationController(
+      vsync: this,
+      duration: Durations.medium4,
+    );
+    _scaleAnimation = Tween<double>(begin: 1, end: 5).animate(
+      CurvedAnimation(parent: _animationController1, curve: Curves.easeInBack),
+    );
+  }
+
+  @override
+  void dispose() {
+    _animationController1.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return SizedBox.expand(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         crossAxisAlignment: CrossAxisAlignment.center,
-        children: [],
+        children: [
+          _animatedContainerExample(),
+        ],
+      ),
+    );
+  }
+
+  Widget _animatedContainerExample() {
+    return SizedBox(
+      width: 500,
+      child: Column(
+        children: [
+          ScaleTransition(
+            scale: _scaleAnimation,
+            child: Container(
+              height: 50,
+              width: 50,
+              color: Colors.red,
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              MaterialButton(
+                onPressed: () {
+                  _animationController1.forward();
+                },
+                child: Text("forward"),
+              ),
+              MaterialButton(
+                onPressed: () {
+                  _animationController1.reverse();
+                },
+                child: Text("rev"),
+              ),
+              MaterialButton(
+                onPressed: () {
+                  _animationController1.stop();
+                },
+                child: Text("stop"),
+              ),
+              MaterialButton(
+                onPressed: () {
+                  _animationController1.reset();
+                },
+                child: Text("reset"),
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
--- lib/pages/implicit_animations_page.dart
+++ lib/pages/implicit_animations_page.dart
@@ -1,3 +1,4 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
 
 class ImplicitAnimationsPage extends StatefulWidget {
@@ -10,6 +11,7 @@ class ImplicitAnimationsPage extends StatefulWidget {
 class _ImplicitAnimationsPageState extends State<ImplicitAnimationsPage> {
   bool trigger = false;
   double blurValue = 0.01;
+
   @override
   Widget build(BuildContext context) {
     return SizedBox.expand(
@@ -31,10 +33,14 @@ class _ImplicitAnimationsPageState extends State<ImplicitAnimationsPage> {
           height: 300,
           width: 300,
           duration: Durations.long4,
+          curve: Curves.easeInOutSine,
           decoration: BoxDecoration(
-              color: trigger ? Colors.yellow : Colors.pink,
-              border: Border.all(
-                  color: trigger ? Colors.green : Colors.orange, width: 20)),
+            color: trigger ? Colors.yellow : Colors.pink,
+            border: Border.all(
+              color: trigger ? Colors.green : Colors.orange,
+              width: 20,
+            ),
+          ),
         ),
         MaterialButton(
           onPressed: () {
@@ -43,7 +49,7 @@ class _ImplicitAnimationsPageState extends State<ImplicitAnimationsPage> {
             });
           },
           child: const Text("Trigger"),
-        )
+        ),
       ],
     );
   }
@@ -53,15 +59,39 @@ class _ImplicitAnimationsPageState extends State<ImplicitAnimationsPage> {
       width: 200,
       child: Column(
         children: [
+          TweenAnimationBuilder(
+            tween: Tween<double>(begin: 0.01, end: blurValue),
+            duration: const Duration(seconds: 8),
+            builder: (BuildContext context, double? value, Widget? child) {
+              return SizedBox(
+                height: 100,
+                width: 100,
+                child: ImageFiltered(
+                  imageFilter: ImageFilter.blur(
+                    sigmaX: value ?? 0.01 * 40,
+                    sigmaY: value ?? 0.01 * 40,
+                  ),
+                  child: const Image(
+                    fit: BoxFit.cover,
+                    image: NetworkImage(
+                      // Use a more reliable, free image URL for testing
+                      'https://picsum.photos/200/300',
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
           Slider(
-              value: blurValue,
-              min: 0.01,
-              max: 10,
-              onChanged: (value) {
-                setState(() {
-                  blurValue = value;
-                });
-              })
+            value: blurValue,
+            min: 0.01,
+            max: 10,
+            onChanged: (value) {
+              setState(() {
+                blurValue = value;
+              });
+            },
+          ),
         ],
       ),
     );
